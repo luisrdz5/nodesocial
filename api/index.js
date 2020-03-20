e82a1a8442d848c1c10ad413881b554769e1f88d
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2020-03-19 19:13:48 -0600

    added the documentation to the project with swagger

--- api/index.js
+++ api/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = require('./components/user/swagger.json');
+
 const config =require('../config.js');
 const user = require('./components/user/network');
+
 
 
 app.use(bodyParser.json());
 //Router
 app.use('/api/user', user);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
     console.log('API listening in port  ', config.api.port);
